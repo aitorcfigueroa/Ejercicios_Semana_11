e1b0b7e505fd1a25163291696ab78f7364c40516
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-20 09:08:54 +0100

    Add Ejercicio 3

---
+++ src/Agenda.java
@@ -0,0 +1,64 @@
+public class Agenda {
+    public static void main(String[] args) {
+        System.out.println("AGENDA");
+        int exitProgram = 1;
+        Perfil [] agenda = new Perfil[5];
+
+        while (exitProgram != 0) {
+            System.out.println("Seleccione una acción: \n1 - Añadir un contacto. \n2 - Consultar agenda. \n0 - Salir.");
+            try {
+                exitProgram = Input.num();
+                Options(exitProgram, agenda);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("Por favor, seleccione una opción válida.");
+                exitProgram = 1;
+            }
+        }
+    }
+
+    public static void Options(int select, Perfil[] agenda) {
+        if (select == 1) {
+            CreateContact(agenda);
+        } else if (select == 2) {
+            ShowAgenda(agenda);
+        } else if (select != 0) {
+            System.out.println("Por favor, seleccione una opción válida");
+        }
+    }
+
+    public static void CreateContact(Perfil[] agenda) {
+        int exitCreateContact = 1;
+
+        while (exitCreateContact != 0) {
+            Perfil newContact = new Perfil();
+            System.out.println(newContact.getData());
+            WriteAgenda(agenda, newContact);
+            System.out.println("Desea crear un nuevo contacto: \n1 - Sí \n0 - no");
+            try {
+                exitCreateContact = Input.num();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                exitCreateContact = 1;
+            }
+        }
+    }
+
+    public static void WriteAgenda(Perfil[] agenda, Perfil newContact) {
+        for (int i = 0; i < 5; i++) {
+            if (agenda[i] == null) {
+                agenda[i] = newContact;
+                break;
+            }
+        }
+    }
+    public static void ShowAgenda(Perfil[] agenda) {
+        for (int i = 0; i < 5; i++) {
+            if (agenda[i] == null) {
+                System.out.println("Empty");
+            } else {
+                System.out.println(agenda[i].getData());
+            }
+        }
+    }
+}
--- src/Perfil.java
+++ src/Perfil.java
@@ -1,5 +1,54 @@
+import java.util.InputMismatchException;
+
 public class Perfil {
-    String name = "";
-    String lastname = "";
-    int phone = 0;
+    private String name = "";
+    private String lastname = "";
+    private int phone = 0;
+
+    Perfil() {
+        System.out.println("Introduzca un nombre:");
+        this.name = Input.str();
+        System.out.println("Introduzca un apellido:");
+        this.lastname = Input.str();
+        System.out.println("Introduzca un teléfono:");
+
+        boolean phoneOk = false;
+        while (!phoneOk) {
+            try {
+                phoneOk = true;
+                this.phone = Input.num();
+            } catch (InputMismatchException ex) {
+                phoneOk = false;
+                System.out.println("Por favor, introduzca un valor válido.");
+            }
+        }
+    }
+
+    public String getData() {
+        return (this.name + " " + this.lastname + ": " + this.phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
 }
